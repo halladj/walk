4aeba4be2b4bdd996b546a08b04f4b1c6e16608e
halladj <halladj00@gmail.com>
2025-03-10 12:41:07 +0100

    implemented filterOut() & listFile()

--- main.go
+++ main.go
@@ -61,8 +61,16 @@ func filterOut(
 	minSize int64,
 	info os.FileInfo,
 ) bool {
+	if info.IsDir() || info.Size() < minSize {
+		return true
+	}
+	if ext != "" && filepath.Ext(path) != ext {
+		return true
+	}
+
 	return false
 }
 func listFile(path string, out io.Writer) error {
-	return nil
+	_, err := fmt.Fprintln(out, path)
+	return err
 }
