134f6c3688d053d384877ba958573677728402c4
halladj <halladj00@gmail.com>
2025-04-09 13:04:07 +0100

    deletion feature

--- main.go
+++ main.go
@@ -13,11 +13,13 @@ type config struct {
 	ext  string
 	size int64
 	list bool
+	del  bool
 }
 
 func main() {
 	root := flag.String("root", ".", "Root directory to start.")
 	list := flag.Bool("list", false, "List files only")
+	del := flag.Bool("del", false, "Delete Files")
 	ext := flag.String("ext", "", "File extentions to filter out")
 	size := flag.Int64("size", 0, "Minimum file size")
 
@@ -27,6 +29,7 @@ func main() {
 		list: *list,
 		ext:  *ext,
 		size: *size,
+		del:  *del,
 	}
 
 	if err := run(*root, os.Stdout, c); err != nil {
@@ -49,6 +52,10 @@ func run(root string, out io.Writer, c config) error {
 
 			if c.list {
 				return listFile(path, out)
+			}
+
+			if c.del {
+				return delFile(path)
 			}
 
 			return listFile(path, out)
